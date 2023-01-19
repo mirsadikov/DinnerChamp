7f10f1c600d183bfb23fb48b62e899d161818c87
Mirabror Sodikov <mirabror9545@gmail.com>
2023-01-19 13:14:45 +0500

    feat: dashboard card

---
+++ website/src/Components/DashboardCard.jsx
@@ -0,0 +1,7 @@
+export default function DashboardCard() {
+  return (
+    <div className="dashboard__card dashboard">
+      <h2 className="dashboard__card-title">Dashboard</h2>
+    </div>
+  );
+}
--- website/src/Screens/Dashboard.jsx
+++ website/src/Screens/Dashboard.jsx
@@ -2,6 +2,7 @@ import { useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import { NavLink, Route, Routes, useNavigate } from 'react-router-dom';
 import Categories from '../Components/Categories';
+import DashboardCard from '../Components/DashboardCard';
 import Dishes from '../Components/Dishes';
 import DishForm from '../Components/DishForm';
 import ProfileCard from '../Components/ProfileCard';
@@ -23,6 +24,9 @@ export default function Dashboard() {
         <div className="dashboard__main dashboard__column">
           <h2 className="dashboard__title">Profile</h2>
           <ProfileCard info={info} />
+          <NavLink to="/dashboard/" exact className="dashboard__card">
+            Dashboard
+          </NavLink>
           <NavLink to="/dashboard/dishes" className="dashboard__card">
             View Dishes
           </NavLink>
@@ -32,6 +36,7 @@ export default function Dashboard() {
         </div>
         <div className="dashboard__block dashboard__column">
           <Routes>
+            <Route path="/" element={<DashboardCard />} />
             <Route path="/profileform" element={<ProfileForm />} />
             <Route path="/dishes" element={<Dishes />} />
             <Route path="/categories" element={<Categories />} />
