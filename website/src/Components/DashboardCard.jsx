47f57259dd561b0a33e892c6d68451302c31e449
Mirabror Sodikov <mirabror9545@gmail.com>
2023-02-09 18:55:18 +0500

    fix: temporary message

--- website/src/Components/DashboardCard.jsx
+++ website/src/Components/DashboardCard.jsx
@@ -1,7 +1,8 @@
 export default function DashboardCard() {
   return (
-    <div className="dashboard__card dashboard">
+    <div className="dashboard__card statistics">
       <h2 className="dashboard__card-title">Dashboard</h2>
+      <div className="dashboard__card-content">TO BE BUILT</div>
     </div>
   );
 }
