6498567cc4145e3870d6dcbdfd83a4f0d907ec89
Mirabror Sodikov <mirabror9545@gmail.com>
2023-03-04 11:45:57 +0500

    fix: small issues

--- client/components/Meta.jsx
+++ client/components/Meta.jsx
@@ -3,7 +3,7 @@ import Head from 'next/head';
 const Meta = ({ title, keywords, description }) => {
   return (
     <Head>
-      <meta charset="utf-8" />
+      <meta charSet="utf-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <meta name="keywords" content={keywords} />
       <meta name="description" content={description} />
