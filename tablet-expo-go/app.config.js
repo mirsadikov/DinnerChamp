47e0460a5b767e850db47b2da900932d2b64f9b0
Mirabror Sodikov <mirabror9545@gmail.com>
2023-04-07 16:57:04 +0500

    fix

--- tablet-expo-go/app.config.js
+++ tablet-expo-go/app.config.js
@@ -4,8 +4,17 @@ const apiURL = process.env.API_URL;
 
 export default {
   expo: {
+    name: "tablet-expo-go",
+    slug: "tablet-expo-go",
+    version: "1.0.0",
+    android: {
+      package: "com.zeyd.dinnerchamptablet"
+    },
     extra: {
       apiUrl: apiURL,
+      eas: {
+        projectId: 'fb2cf6b4-056f-41c5-9566-82451d69ab23',
+      },
     },
-  }
-}
+  },
+};
